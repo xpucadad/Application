690473aa2e6e2cf7fd2a5397fb1089ce4750a63d
Kenneth Reed <ken+git@kmreed.net>
2014-06-10 19:18:43 -0700

    Commit changes before merging from master

--- lib/Properties.pm
+++ lib/Properties.pm
@@ -1,3 +1,32 @@
 package Properties;
+use strict;
+use warnings;
 
+use base qw(BaseObject);
+
+# Project objects
+use Log;
+
+sub new($$) {
+  my $class = shift;
+  my $file = shift;
+  my $self = new BaseObject();
+  bless($self, $class);
+  return $self->load_properties($file);
+}
+
+sub load_properties($$) {
+  my $self = shift;
+  my $filename = shift;
+
+  if (open(PROPS,"./config/$filename")) {
+
+  }
+  else {
+    my $log = $self->{log};
+    $log->add_entry("Failed to load properties files $filename. Error: $!\n");
+    $self = 0;
+  }
+  return $self;
+}
 1;
